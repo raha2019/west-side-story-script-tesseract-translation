c1b40fe5a059258d158ed04430b802004ff3ee13
Rahul Gupta <47731685+raha2019@users.noreply.github.com>
2019-04-02 19:02:53 -0400

    Added all content

---
+++ West-Side.py
@@ -0,0 +1,383 @@
+import pytesseract
+from PIL import Image
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+input = "West-Side-Story-script-004.jpg"
+
+output = pytesseract.image_to_string(Image.open(input))
+print(output)
+
+input = "West-Side-Story-script-005.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-006.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-007.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-008.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-009.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-010.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-011.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-012.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-013.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-014.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-015.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-016.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-017.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-018.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-019.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-020.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-021.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-022.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-023.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-024.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-025.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-026.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-027.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-028.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-029.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-030.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-031.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-032.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-033.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-034.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-035.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-036.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-037.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-038.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-039.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-040.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-041.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-042.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-043.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-044.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-045.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-046.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-047.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-048.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-049.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-050.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-051.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-052.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-053.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-054.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-055.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-056.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-057.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-058.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-059.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-060.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-061.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-062.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-063.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-064.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-065.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-066.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-067.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-068.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-069.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-070.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-071.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-072.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-073.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-074.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-075.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-076.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-077.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-078.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-079.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-080.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-081.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-082.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-083.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-084.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-085.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-086.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-087.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-088.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-089.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-090.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-091.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-092.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-093.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-094.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-095.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-096.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-097.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-098.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-099.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-100.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-101.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-102.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-103.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-104.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-105.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-106.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-107.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-108.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-109.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-110.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-111.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-112.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-113.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-114.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-115.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-116.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-117.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-118.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-119.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-120.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-121.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-122.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-123.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-124.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-125.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-126.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-127.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
+input = "West-Side-Story-script-128.jpg"
+output = pytesseract.image_to_string(Image.open(input))
+print (output)
